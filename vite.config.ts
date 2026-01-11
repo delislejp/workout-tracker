306d4d8cccbce9cfce5a5d3685d5f7d4f6f334c3
Jean-Philippe Delisle <delisle_jp@outlook.com>
2026-01-10 19:18:57 -0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/workout-tracker/',
   plugins: [react()],
   css: {
     postcss: {
